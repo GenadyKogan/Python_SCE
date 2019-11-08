65faa6cedaf01052d92743be7d6e1e64b0b6f74b
rgkog <rgkog@DESKTOP-GI2KK2V>
2019-11-08 04:34:50 +0200

    task 5 not complete

--- HW_2019/src/HW_1_328956537_.py
+++ HW_2019/src/HW_1_328956537_.py
@@ -1,28 +1,28 @@
-
 '''====================================================================='''
 # task 1
+from _ast import Num
 def Younger():
-    nameA  = input("Enter a  name of the first person ")
-    print("Enter a  birthdate of " + nameA)
-    dayA = input("Day: ")
-    monthA = input("Month: ")
-    yearA  = input("Year: ")
+    nameA  = input('Enter a  name of the first person ')
+    print('Enter a  birthdate of ' + nameA)
+    dayA = input('Day: ')
+    monthA = input('Month: ')
+    yearA  = input('Year: ')
     
-    nameB  = input("Enter a  name of the second person ")
-    print("Enter a  birthdate of " + nameB)
-    dayB = input("Day: ")
-    monthB = input("Month: ")
-    yearB = input("Year: ")
+    nameB  = input('Enter a  name of the second person ')
+    print('Enter a  birthdate of ' + nameB)
+    dayB = input('Day: ')
+    monthB = input('Month: ')
+    yearB = input('Year: ')
     
-    if yearA > yearB:print(nameA + " is younger")
-    elif yearA < yearB:  print(nameB + " is younger")
+    if yearA > yearB:print(nameA + ' is younger')
+    elif yearA < yearB:  print(nameB + ' is younger')
     elif yearA == yearB:
-        if monthA>monthB:print(nameA + " is younger")
-        elif monthA < monthB:  print(nameB + " is younger") 
+        if monthA>monthB:print(nameA + ' is younger')
+        elif monthA < monthB:  print(nameB + ' is younger') 
         elif monthA == monthB:
-            if dayA>dayB:print(nameA + " is younger")
-            elif dayA<dayB:print(nameB + " is younger")
-            else:print(nameA +" and " + nameB + " are same age")
+            if dayA>dayB:print(nameA + ' is younger')
+            elif dayA<dayB:print(nameB + ' is younger')
+            else:print(nameA +' and ' + nameB + ' are same age')
 #Younger()
 '''====================================================================='''
 # task 2
@@ -57,37 +57,6 @@ def PrintFigure(num):
     chr(92) = \
     chr(124) = |
     """
-    #rownum = (num//2)+1
-   
-    #i =0
-    #j= num-1
-    
-    #colnum = num
-    '''==================================='''
-    #for row in range (rownum):
-        #for col in range(colnum):
-            # \
-            #if row == col and row!=rownum-1 and col!=rownum-1:
-                #print(chr(92),end = "")
-            # *
-            #elif row == col :
-                #print("*",end = "")
-            # |    
-            #elif col == rownum - 1:
-                #print(chr(124),end = "")
-            # /
-            #elif row ==i  and col == j:
-               # print(chr(47),end = "")
-                #i = i+1
-                #j = j-1
-            # -
-            #elif row == rownum-1:
-                #print("-", end="")
-            
-            #else: print(end = " ")
-            
-        #print()
-     
     i=0
     j=num-1
     for row in range (num):
@@ -95,60 +64,77 @@ def PrintFigure(num):
             # /
             if row == i and col == j:
                 if row == col:
-                    print("*",end = "")
+                    print('*',end = '')
                 
-                else:print(chr(47),end= "")
+                else:print(chr(47),end= '')
                 i+=1
                 j-=1
             # \   
             elif row  == col:
-                print(chr(92),end ="")
+                print(chr(92),end ='')
             # |
             elif col == (num//2):
-                    print(chr(124),end = "")
+                    print(chr(124),end = '')
             # -
             elif row == (num//2):
-                print("-", end="")
+                print('-', end='')
             else:
-                print(end=" ")
+                print(end=' ')
         print()
                    
-   
-    '''==================================='''
-   
-    '''  n= (n//2)+1
-    k = 0
-    for i in range(n - 1, -1, -1): 
-  
-        
-        for j in range(n - 1, k, -1): 
-            print(' ', end = '') 
-  
-        if n== 4 and i ==3 and j ==1  and k == 0:
-            print("*", end='')
-        else: print(chr(47), end = '') 
-  
-         
-        for j in range(1, k * 2): 
-            print(' ', end = '') 
-        if i<n-1: 
-            print(chr(92), end = '') 
-        print() 
-        k += 1'''
-            
 
 def Main():
-    print("Enter positive and not even number: ")
+    print('Enter positive and not even number: ')
     while True:
         num = int(input())
         if num < 0 or num %2 ==0:
-            print("Wrong number.Enter positive and not even number: ")
+            print('Wrong number.Enter positive and not even number: ')
             continue
         else:
             break
     
     PrintFigure(num)
 
-Main()
+#Main()
+'''=====================================================================''' 
 
+# task 5
+
+
+def Factorial(num):
+    '''Function to calculate the factorial of any'''
+    f = 1;
+    while(num!=0):
+        f=f*num
+        num -=1
+    return f
+
+def SumOfFactorials(num):
+    '''Function check if number = sum of factorials digits'''
+    sum =0
+    temp = num
+    while(temp!=0):
+        '''Calculate factorial of last digit'''
+        sum = sum + Factorial(temp%10)
+        '''Replace value of temp by temp / 10''' 
+        temp = temp // 10;
     
+    return (sum == num)
+
+
+def Test():
+    print('Enter positive integer number: ')
+    while True:
+        num = int(input())
+        if num < 0:
+            print('Wrong number.Enter integer number: ')
+            continue
+        else:
+            break
+        
+    if SumOfFactorials(num):
+        print("Yes")
+    else:
+        print("No")
+   
+Test()
