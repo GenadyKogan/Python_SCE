ca252d6f80d454eb552bbd455da7547d76ce2ad4
rgkog <rgkog@DESKTOP-GI2KK2V>
2019-11-27 13:01:30 +0200

    #task 4

---
+++ HW_2019/src/HW_2.py
@@ -0,0 +1,29 @@
+#task 4
+def Make_iterator(fn):
+    x=0
+    def foo(): 
+        nonlocal x 
+        '''for _ in range(x):
+            
+            print("res", x)'''
+        res = fn(x)
+        x+=1
+        return res
+    return foo
+
+fn = lambda n:2*n
+iterator = Make_iterator(fn)
+
+for i in range(4):     
+    print("iterator",iterator())
+print('==========================')   
+for i in range(2,4):     
+    print("iterator", iterator())
+print('==========================') 
+it = Make_iterator(fn)
+for i in range(2):
+    print("it" ,it())
+ 
+print('==========================')   
+for i in range(2,4):     
+    print("iterator",iterator())
---
+++ LabExercise/src/lab_3.py
@@ -0,0 +1,10 @@
+def g(u):
+    
+    print(u+1)
+    def f(i):
+        
+        
+        return i
+    return f(10)
+
+print(g(20))
