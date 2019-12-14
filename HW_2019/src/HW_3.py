c90ff9270ca750d148c5aeb09a8f636b041aca22
rgkog <rgkog@DESKTOP-GI2KK2V>
2019-12-14 15:53:44 +0200

    hw 3 task 2 count value

--- HW_2019/src/HW_3.py
+++ HW_2019/src/HW_3.py
@@ -1,5 +1,9 @@
-# task 1
 
+#===============================================
+
+                # task 1
+
+#===============================================
 
 def make_power(x,y):
     def dispatch(i):
@@ -15,12 +19,12 @@ def base(x):return getitePower(x,0)
 def power(x):return getitePower(x,1)
 #===============================================
 def print_power(x):
-    if x>0 and x<1:
+    if power(x)>0 and power(x)<1:
         return x
-    if power(x)==0 or power(x)==1:return '{0}'.format(base(x))     
-    if power(x)==2:return '{0}'.format(base(x)*base(x))  
+    if power(x)==0 or power(x)==1:print('{0}'.format(base(x)) )    
+    if power(x)==2:print('{0}'.format(base(x)*base(x)) ) 
     #return '{0}{1}{2}'.format(base(x),(lambda x:'^' if x>=0 else '^')(power(x)),power(x))
-    return '{0}{1}{2}'.format(base(x),'^',power(x))  
+    print('{0}{1}{2}'.format(base(x),'^',power(x)))  
 #===============================================
 def improve_power(x):
     return make_power(returnBase(base(x)), amountOfPower(base(x))*power(x))
@@ -29,11 +33,11 @@ import math
 def power_function(decimal, integer):
     num=1
     if integer>0:
-        for function in range(integer):
+        for i in range(integer):
             num=num*decimal
     if integer<0:
         num=1.0 # force floating point division
-        for function in range(-integer):
+        for i in range(-integer):
             num=num/decimal
     return num
 
@@ -84,7 +88,6 @@ def isPower(n) :
     return False
 #print(isPower(19683))
 #===============================================
-
 def mul_power(x,y):
     a=power_function(base(x),power(x))
     b=power_function(base(y),power(y))
@@ -94,7 +97,7 @@ def mul_power(x,y):
     #return res
     return make_power(returnBase(res), amountOfPower(res))
 
-
+#===============================================
 def div_power(x,y):
     a=power_function(base(x),power(x))
     b=power_function(base(y),power(y))
@@ -102,23 +105,74 @@ def div_power(x,y):
         return make_power(returnBase(a), amountOfPower(a)-amountOfPower(b))
     
     res=a/b
-    if res>0 and res<1:
-        return print_power(res)
+    #print("res-->",res)
+    #if res>0 and res<1:
+        #return print_power(res)
     return make_power(returnBase(res), amountOfPower(res))
+#===============================================
 
-x= make_power(4,-5)
+#x= make_power(4,5)
 
 #print(x)
 #print(base(x))
 #print(power(x))
 #print(print_power(x))
 #print(print_power(improve_power(x)))
-y=make_power(9,2)
+#y=make_power(9,2)
 #print(print_power(improve_power(y))) 
 #print(print_power(mul_power(y,x))) 
-#print(print_power(mul_power(improve_power(y),make_power(3,5)))) 
-print(print_power(div_power(improve_power(y),make_power(4,5))))
+#print_power(mul_power(improve_power(y),make_power(3,5)))
+
 #print(print_power(div_power(mul_power(make_power(2,3),make_power(2,8)), make_power(2,4))))
 #print( print_power(div_power(mul_power(improve_power(make_power(8,1)), improve_power(make_power(256,1))),improve_power(make_power(16,1)))))
 #print(print_power(make_power(12,1))) 
 #print(print_power(make_power(12,0))) 
+
+#===============================================
+
+                # task 2
+
+#===============================================
+
+def make_tree(value,left,right):
+    def dispatch(message):
+        if message=='value':return value
+        if message=='right_node':return right
+        if message=='left_node':return left
+    return dispatch
+#===============================================
+def getitem_node(f,i):return f(i)
+#===============================================
+def right(node):
+    #print(node)
+    return getitem_node(node, 'right_node')
+#===============================================
+def left(node):
+    return  getitem_node(node,'left_node')
+#===============================================
+def value(node):
+    return getitem_node(node, 'value')
+    #return node("value")
+#===============================================
+def print_tree(tree):
+    if tree!=None:
+        print_tree(left(tree))
+        print(value(tree),end=" ")
+        print_tree(right(tree))
+        
+#===============================================
+def count_value(tree,val):
+    if tree==None:
+        return 0
+    if value(tree)==val:
+        return 1+count_value(left(tree),val)+ count_value(right(tree),val)
+    return 0+count_value(left(tree),val)+ count_value(right(tree),val)
+    
+
+    
+tree1=make_tree(12,   make_tree(6,make_tree(8,None,None),None),     make_tree(7,make_tree(8,None,None),make_tree(15,None,None) ) )
+#tree2=make_tree(12,make_tree(6,make_tree(3,make_tree(1,None,None),None),make_tree(8,make_tree(7,None,None),None)),make_tree(15,None,make_tree(20,make_tree(17,None, None),None))) 
+#print_tree(tree1)
+#print_tree(tree1) 
+#print(value(left((tree1)))) 
+print(count_value(tree1,12))
